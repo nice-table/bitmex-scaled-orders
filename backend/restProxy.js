453519df2752ffebdc20e186ef1de61db0da7817
nice-table <39131029+nice-table@users.noreply.github.com>
2018-07-06 13:21:59 +0200

    Today I Learned that Firefox doesn't like wildcards

--- backend/restProxy.js
+++ backend/restProxy.js
@@ -59,8 +59,14 @@ app.use(function(req, res) {
   delete req.headers["host"];
 
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-type, Content-Length, api-expires,api-key,api-signature"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,POST,PUT,DELETE,OPTIONS"
+  );
 
   proxy.web(req, res, { target: apiURL });
 });
